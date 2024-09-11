39a7a0c15359df1a56aab32abb37eaa2e623ada1
zabackary <137591653+zabackary@users.noreply.github.com>
2024-09-11 14:02:29 +0900

    chore: simplify setup

--- src/main.py
+++ src/main.py
@@ -1,49 +1,12 @@
 from vex import *
 
-from constants import AUTONOMOUS_ROUTE, COMPETITION_MODE, USE_REAL_BOT
-from driver_control import driver_control
-from peripherals import RealBotPeripherals, TestBotPeripherals
-from routes import routes
-from utils import has_interaction
+from peripherals import RobotPeripherals
 
-selected_autonomous: str = AUTONOMOUS_ROUTE
+perfs = RobotPeripherals()
 
-if USE_REAL_BOT:
-    peripherals = RealBotPeripherals()
-else:
-    peripherals = TestBotPeripherals()
-drivetrain = SmartDrive(
-    lm=peripherals.left_motors,
-    rm=peripherals.right_motors,
-    wheelTravel=peripherals.WHEEL_TRAVEL_MM,
-    trackWidth=peripherals.WHEEL_TRACK_WIDTH_MM,
-    g=peripherals.inertial
-)
+# Write your code here...
 
-
-def autonomous():
-    route = None
-    for possible_route in routes:
-        if possible_route.name() == selected_autonomous:
-            route = possible_route
-    if route is None:
-        raise Exception("undefined route! {}".format(route))
-    route.run(peripherals, drivetrain)
-
-
-def driver():
-    # don't run driver control if the controller is disabled
-    while not has_interaction(peripherals):
-        pass
-    # keep running the driver control loop if it crashes for redundancy
-    while True:
-        try:
-            driver_control(peripherals)
-        except Exception:
-            pass
-
-
-if COMPETITION_MODE:
-    Competition(driver, autonomous)
-else:
-    autonomous()
+# You're using vexmason, so you can import modules you made or ones you found
+# on [PyPI] now! (Some modules on PyPI with C++ dependencies are not supported)
+#
+# [PyPI]: https://pypi.org/
--- src/peripherals.py
+++ src/peripherals.py
@@ -8,68 +8,32 @@ class Peripherals:
     left_motors: MotorGroup
     right_motors_list: list[Motor]
     right_motors: MotorGroup
-    controller: Controller
-    claw_piston: Pneumatics
-    wing_piston: Pneumatics
-    front_sonar: Sonar
 
     WHEEL_TRAVEL_MM: int
     WHEEL_TRACK_WIDTH_MM: int
 
 
-class RealBotPeripherals(Peripherals):
+class RobotPeripherals(Peripherals):
+    """Peripheral config for the robot.
+
+    This is a _subclass_ of Peripherals. This is because there can be many
+    different _types_ of Peripherals (for example, we had a test bot last year
+    as well as a real one), so subclasses let us express that relationship.
+    """
+
     WHEEL_TRAVEL_MM = 460
     WHEEL_TRACK_WIDTH_MM = 305
 
     def __init__(self) -> None:
         self.brain = Brain()
         self.controller = Controller()
-
-        self.wing_piston = Pneumatics(self.brain.three_wire_port.h)
-        self.claw_piston = Pneumatics(self.brain.three_wire_port.g)
-
-        self.front_sonar = Sonar(self.brain.three_wire_port.c)
-
         self.inertial = Inertial(Ports.PORT6)
 
         self.left_motors_list = [
-            Motor(Ports.PORT20, False),
-            Motor(Ports.PORT19, True),
-            Motor(Ports.PORT9, False),
-            Motor(Ports.PORT8, True),
+            # Left motors list here
         ]
         self.left_motors = MotorGroup(*self.left_motors_list)
         self.right_motors_list = [
-            Motor(Ports.PORT12, True),
-            Motor(Ports.PORT13, False),
-            Motor(Ports.PORT1, True),
-            Motor(Ports.PORT2, False),
-        ]
-        self.right_motors = MotorGroup(*self.right_motors_list)
-
-
-class TestBotPeripherals(Peripherals):
-    WHEEL_TRAVEL_MM = 320
-    WHEEL_TRACK_WIDTH_MM = 265
-
-    def __init__(self) -> None:
-        self.brain = Brain()
-        self.controller = Controller()
-
-        self.wing_piston = Pneumatics(self.brain.three_wire_port.f)
-        self.claw_piston = Pneumatics(self.brain.three_wire_port.a)
-
-        self.front_sonar = Sonar(self.brain.three_wire_port.c)
-
-        self.inertial = Inertial(Ports.PORT5)
-
-        self.left_motors_list = [
-            Motor(Ports.PORT20),
-            Motor(Ports.PORT19)
-        ]
-        self.left_motors = MotorGroup(*self.left_motors_list)
-        self.right_motors_list = [
-            Motor(Ports.PORT11, True),
-            Motor(Ports.PORT1, True)
+            # Right motors list here
         ]
         self.right_motors = MotorGroup(*self.right_motors_list)
